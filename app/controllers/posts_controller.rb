11a6934f75083e16c1daf2e3070bed72097ddde7
Wasif Khan <waasif.khaan@gmail.com>
2018-10-15 17:00:46 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
     @authors = Author.all
     
     if !params[:author].blank?
-      @posts = Post.where(author: params[:author])
+      @posts = Post.by_author(params[:author])
     elsif !params[:date].blank?
       if params[:date] == "Today"
         @posts = Post.where("created_at >=?", Time.zone.today.beginning_of_day)
